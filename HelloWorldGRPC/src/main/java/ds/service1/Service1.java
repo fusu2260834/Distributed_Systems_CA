12bb30acbd8a6a639364171cfff1c5027f9859b3
fusu2260834 <163172749+fusu2260834@users.noreply.github.com>
2024-08-10 22:02:19 +0100

    Update Service1.java

--- HelloWorldGRPC/src/main/java/ds/service1/Service1.java
+++ HelloWorldGRPC/src/main/java/ds/service1/Service1.java
@@ -7,68 +7,62 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
+public class Service1 extends Service1ImplBase {
 
-public class Service1 extends Service1ImplBase{
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Service1 service1 = new Service1();
 
+        int port = 50051;
+        Server server;
 
+        try {
+            server = ServerBuilder.forPort(port)
+                    .addService(service1)
+                    .build()
+                    .start();
+        } catch (IOException e) {
+            System.out.println("Service-1 failed to connect. Is the port already in use?");
+            return;
+        }
 
-	public static void main(String[] args) throws InterruptedException, IOException {
-		Service1 service1 = new Service1();
+        System.out.println("Service-1 started, listening on " + port);
 
-		int port = 50051;
-		Server server;
-		
-		try
-		{
-			server = ServerBuilder.forPort(port)
-					.addService(service1)
-					.build()
-					.start();
-		}
-		catch(IOException e)
-		{
-			System.out.println("Service-1 failed to connect. Is the port already in use?");
-			return;
-		}
+        try {
+            server.awaitTermination();
+        } catch (InterruptedException e) {
+            System.out.println("Service-1 was interrupted.");
+        }
+    }
 
-		System.out.println("Service-1 started, listening on " + port);
+    int Roomtemperature = 25;
 
-		try
-		{
-			server.awaitTermination();
-		}
-		catch(InterruptedException e)
-		{
-			System.out.println("Service-1 was interrupted.");
-		}
-	}
+    @Override
+    public void service1Do(RequestMessage request, StreamObserver<ResponseMessage> responseObserver) {
 
-	int Roomtemperature = 25;
+        String responseMessage;
 
-	@Override
-	public void service1Do(RequestMessage request, StreamObserver<ResponseMessage> responseObserver) {
+        try {
+            // Attempt to parse the temperature as an integer
+            int temperature = Integer.parseInt(request.getText());
 
-		//prepare the value to be set back
-		int temperature = Integer.parseInt(request.getText());
-		String responseMessage = null;
-		if(temperature > 22 & temperature < 28)
-		{
-			responseMessage = "RoomTemperature is within range";
-		}
-		else 
-		{
-			responseMessage = "RoomTemperature is out of range,please check your air conditioner immediatly.";
-		}
+            // Check if the temperature is within the specified range
+            if (temperature > 22 && temperature < 28) {
+                responseMessage = "RoomTemperature is within range";
+            } else {
+                responseMessage = "RoomTemperature is out of range, please check your air conditioner immediately.";
+            }
+        } catch (NumberFormatException e) {
+            // Handle invalid number format
+            responseMessage = "Invalid input: Temperature must be a number.";
+        }
 
+        // Prepare the response message
+        ResponseMessage reply = ResponseMessage.newBuilder().setResponse(responseMessage).build();
 
-		//preparing the response message
-		ResponseMessage reply = ResponseMessage.newBuilder().setResponse(responseMessage).build();
+        // Send the response back to the client
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
 
-		responseObserver.onNext( reply ); 
-
-		responseObserver.onCompleted();
-		
-		System.out.println("Sent data");
-
-	}
+        System.out.println("Sent data");
+    }
 }
