d2c090c8424413b22307866e85bf8a8a34bded6c
fusu2260834 <163172749+fusu2260834@users.noreply.github.com>
2024-08-10 22:02:56 +0100

    Update Service2.java

--- HelloWorldGRPC/src/main/java/ds/service2/Service2.java
+++ HelloWorldGRPC/src/main/java/ds/service2/Service2.java
@@ -1,8 +1,6 @@
 package ds.service2;
 
 import java.io.IOException;
-import java.net.SocketException;
-
 import ds.service2.RequestMessage;
 import ds.service2.ResponseMessage;
 import ds.service2.Service2Grpc.Service2ImplBase;
@@ -10,72 +8,62 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 import io.grpc.stub.StreamObserver;
 
+public class Service2 extends Service2ImplBase {
 
-public class Service2 extends Service2ImplBase{
+    public static void main(String[] args) throws InterruptedException, IOException {
+        Service2 service2 = new Service2();
 
+        int port = 50052;
+        Server server;
 
+        try {
+            server = ServerBuilder.forPort(port)
+                    .addService(service2)
+                    .build()
+                    .start();
+        } catch (IOException e) {
+            System.out.println("Service-2 failed to connect. Is the port already in use?");
+            return;
+        }
 
-	public static void main(String[] args) throws InterruptedException, IOException {
-		Service2 service1 = new Service2();
+        System.out.println("Service-2 started, listening on " + port);
 
-		int port = 50052;
-Server server;
-		
-		try
-		{
-			server = ServerBuilder.forPort(port)
-					.addService(service1)
-					.build()
-					.start();
-		}
-		catch(IOException e)
-		{
-			System.out.println("Service-2 failed to connect. Is the port already in use?");
-			return;
-		}
-		
+        try {
+            server.awaitTermination();
+        } catch (InterruptedException e) {
+            System.out.println("Service-2 was interrupted.");
+        }
+    }
 
-		System.out.println("Service-2 started, listening on " + port);
+    int HeartBeatsPM = 60;
 
-		try
-		{
-			server.awaitTermination();
-		}
-		catch(InterruptedException e)
-		{
-			System.out.println("Service-2 was interrupted.");
-		}
+    @Override
+    public void service2Do(RequestMessage request, StreamObserver<ResponseMessage> responseObserver) {
 
-	}
+        String responseMessage;
 
-		//server.awaitTermination();
-	
+        try {
+            // Attempt to parse the HeartBeatsPM as an integer
+            int heartBeatsPM = Integer.parseInt(request.getText());
 
-	int HeartBeatsPM = 60;
+            // Check if the heartbeats per minute are within the specified range
+            if (heartBeatsPM > 60 && heartBeatsPM < 90) {
+                responseMessage = "HeartBeats is within range";
+            } else {
+                responseMessage = "HeartBeats is out of range, please call the nurse team immediately.";
+            }
+        } catch (NumberFormatException e) {
+            // Handle invalid number format
+            responseMessage = "Invalid input: HeartBeats must be a number.";
+        }
 
-	@Override
-	public void service2Do(RequestMessage request, StreamObserver<ResponseMessage> responseObserver) {
+        // Prepare the response message
+        ResponseMessage reply = ResponseMessage.newBuilder().setResponse(responseMessage).build();
 
-		//prepare the value to be set back
-		int HeartBeatsPM = Integer.parseInt(request.getText());
-		String responseMessage = null;
-		if(HeartBeatsPM > 60 & HeartBeatsPM < 90)
-		{
-			responseMessage = "HeartBeats is within range";
-		}
-		else 
-		{
-			responseMessage = "HeartBeats is out of range, please call nurse team immediately.";
-		}
+        // Send the response back to the client
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
 
-		//preparing the response message
-		ResponseMessage reply = ResponseMessage.newBuilder().setResponse(responseMessage).build();
-
-		responseObserver.onNext( reply ); 
-
-		responseObserver.onCompleted();
-		
-		System.out.println("Sent data");
-
-	}
+        System.out.println("Sent data");
+    }
 }
